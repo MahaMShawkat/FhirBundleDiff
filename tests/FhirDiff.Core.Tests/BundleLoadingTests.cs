d5b86825e21f3e61af6616288c2210028ae8cf4e
MahaHub <maha.mars20@gmai.com>
2026-08-09 03:07:44 -0400

    Test: add bundle loading test using Firely SDK

---
+++ tests/FhirDiff.Core.Tests/BundleLoadingTests.cs
@@ -0,0 +1,23 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace FhirDiff.Core.Tests
+{
+    public class BundleLoadingTests
+    {
+        private const string fileName = "Aaron697_Brekke496_2fa15bc7-8866-461a-9000-f739e425860a.json";
+
+        [Fact]
+        public void LoadBundle_ParsesSuccessfully()
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, "TestData", fileName);
+            var parser = new FhirJsonParser();
+            var json = File.ReadAllText(path);
+
+            var bundle = parser.Parse<Bundle>(json);
+
+            Assert.NotNull(bundle);
+            Assert.True(bundle.Entry.Count > 0);
+        }
+    }
+}
