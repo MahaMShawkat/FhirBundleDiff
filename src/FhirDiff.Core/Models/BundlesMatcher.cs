71ae8bd711638d76e08438e3f9f6cb2300e9af3c
MahaHub <maha.mars20@gmai.com>
2026-08-10 02:03:49 -0400

    feat: add getBundleResource method to bundleMatcher class

---
+++ src/FhirDiff.Core/Models/BundlesMatcher.cs
@@ -0,0 +1,24 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirDiff.Core.Models
+{
+    public class BundlesMatcher
+    {
+        public Dictionary<ResourceKey, Resource> GetBundleResources(Bundle bundle)
+        {
+            Dictionary<ResourceKey, Resource> bundleResources = new Dictionary<ResourceKey, Resource>();
+
+            foreach (var entry in bundle.Entry)
+            {
+                var resourceKey = new ResourceKey(entry.Resource.TypeName, entry.Resource.Id);
+                bundleResources.Add(resourceKey, entry.Resource);
+
+            }
+            return bundleResources;
+        }
+
+    }
+}
