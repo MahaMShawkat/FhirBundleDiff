099ae0ae4b626a7d4b6dee61dbd5ac7a30b7a584
MahaHub <maha.mars20@gmai.com>
2026-08-10 03:23:59 -0400

    fix: guard against resources with null Id in GetBundleResources

--- src/FhirDiff.Core/Models/BundlesMatcher.cs
+++ src/FhirDiff.Core/Models/BundlesMatcher.cs
@@ -13,9 +13,13 @@ namespace FhirDiff.Core.Models
 
             foreach (var entry in bundle.Entry)
             {
-                var resourceKey = new ResourceKey(entry.Resource.TypeName, entry.Resource.Id);
-                bundleResources.Add(resourceKey, entry.Resource);
-
+                if (entry.Resource.Id is null)
+                    throw new InvalidOperationException($"{entry.Resource.TypeName} Entry has no Id.Cannot match.");
+                else
+                {
+                    var resourceKey = new ResourceKey(entry.Resource.TypeName, entry.Resource.Id);
+                    bundleResources.Add(resourceKey, entry.Resource);
+                }
             }
             return bundleResources;
         }
