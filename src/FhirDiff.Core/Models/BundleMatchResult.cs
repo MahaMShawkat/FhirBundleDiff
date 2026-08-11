1dad1c7a4c1fe8311eaae5c6bfdabeb76fdacf7c
MahaHub <maha.mars20@gmai.com>
2026-08-11 03:17:17 -0400

    feat: add BundleMatchResult record

---
+++ src/FhirDiff.Core/Models/BundleMatchResult.cs
@@ -0,0 +1,12 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirDiff.Core.Models
+{
+    public record BundleMatchResult(
+    IReadOnlyList<ResourceKey> Added,
+    IReadOnlyList<ResourceKey> Removed,
+    IReadOnlyList<(ResourceKey Key, Resource Old, Resource New)> Matched);
+}
