3c3b1d55c9df33bd68253b9a01a4ef6f204b9ea0
MahaHub <maha.mars20@gmai.com>
2026-08-11 07:47:34 -0400

    feat: add BundleResources record

---
+++ src/FhirDiff.Core/Models/BundleResources.cs
@@ -0,0 +1,11 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirDiff.Core.Models
+{
+    public record BundleResources(
+    IReadOnlyDictionary<ResourceKey, Resource> ResourcesWithId,
+    IReadOnlyList<Resource> ResourcesWithoutId);
+}
