842c7a9ca6949aa5b9eca2010a822869f39adf6b
MahaHub <maha.mars20@gmai.com>
2026-08-11 11:40:19 -0400

    feat: implement matching method for resources with ID

--- src/FhirDiff.Core/Models/BundlesMatcher.cs
+++ src/FhirDiff.Core/Models/BundlesMatcher.cs
@@ -27,5 +27,32 @@ namespace FhirDiff.Core.Models
             return new BundleResources(resourcesWithId, resourcesWithoutId);
         }
 
+        public BundleMatchResult Match(Bundle oldBundle, Bundle newBundle)
+        {
+            List<ResourceKey> listAddedResources = new List<ResourceKey>();
+            List<ResourceKey> listRemovedResources = new List<ResourceKey>();
+            List<(ResourceKey Key, Resource Old, Resource New)> listMatchedResources = new List<(ResourceKey Key, Resource Old, Resource New)>();
+
+            IReadOnlyDictionary<ResourceKey, Resource> oldResourcesWithIdList = GetBundleResources(oldBundle).ResourcesWithId;
+            IReadOnlyDictionary<ResourceKey, Resource> newResourcesWithIdList = GetBundleResources(newBundle).ResourcesWithId;
+
+            foreach (var oldResource in oldResourcesWithIdList)
+            {
+                ResourceKey key = oldResource.Key;
+                if (!newResourcesWithIdList.ContainsKey(key))
+                    listRemovedResources.Add(key);
+                else
+                    listMatchedResources.Add((key, oldResource.Value, newResourcesWithIdList[key]));
+            }
+
+            foreach (var newResource in newResourcesWithIdList)
+            {
+                ResourceKey key = newResource.Key;
+                if (!oldResourcesWithIdList.ContainsKey(key))
+                    listAddedResources.Add(key);
+            }
+
+            return new BundleMatchResult(listAddedResources, listRemovedResources, listMatchedResources);
+        }
     }
 }
