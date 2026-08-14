a3f778e1f675310ec8a4a6047ea767c7891788b1
MahaHub <maha.mars20@gmai.com>
2026-08-14 13:04:02 -0400

    refactor: add ResourceChange class constructors

--- src/FhirDiff.Core/Models/ResourceChange.cs
+++ src/FhirDiff.Core/Models/ResourceChange.cs
@@ -10,6 +10,16 @@
 
     public class ResourceChange
     {
+        public ResourceChange() { }
+
+        public ResourceChange(string resourceType, string resourceId, ChangeType changeType, List<FieldDiff> fieldChanges)
+        {
+            ResourceType = resourceType;
+            ResourceId = resourceId;
+            ChangeType = changeType;
+            FieldChanges = fieldChanges;
+        }
+
         public string ResourceType { get; set; }
         public string ResourceId { get; set; }
         public ChangeType ChangeType { get; set; }
