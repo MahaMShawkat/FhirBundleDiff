5e9139cf9a80e1b9704e9114df1946722bd5003f
MahaHub <maha.mars20@gmai.com>
2026-08-14 13:03:04 -0400

    refactor: add FieldDiff class constructor

--- src/FhirDiff.Core/Models/FieldDiff.cs
+++ src/FhirDiff.Core/Models/FieldDiff.cs
@@ -4,6 +4,13 @@ namespace FhirDiff.Core.Models
 {
     public class FieldDiff
     {
+        public FieldDiff(string fieldPath, JsonElement? oldValue, JsonElement? newValue)
+        {
+            FieldPath = fieldPath;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
         public string FieldPath { get; set; }           // "" = whole resource (used for Added/Removed)
         public JsonElement? OldValue { get; set; }
         public JsonElement? NewValue { get; set; }
