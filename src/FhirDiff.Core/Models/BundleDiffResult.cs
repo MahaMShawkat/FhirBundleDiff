ac85b54a73747357bcebf00c609211f11be64b52
MahaHub <maha.mars20@gmai.com>
2026-08-23 03:37:51 -0400

    feat: add BundleDiffResult constructor with public Changes and NoIdCount

--- src/FhirDiff.Core/Models/BundleDiffResult.cs
+++ src/FhirDiff.Core/Models/BundleDiffResult.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace FhirDiff.Core.Models
 {
     public class BundleDiffResult
     {
-        private List<ResourceChange> Changes { get; set; }
+        public BundleDiffResult(List<ResourceChange> changes)
+        {
+            Changes = changes;
+        }
+
+        public List<ResourceChange> Changes { get; set; }
         public int AddedCount => Changes.Count(c => c.ChangeType == ChangeType.Added);
         public int RemovedCount => Changes.Count(c => c.ChangeType == ChangeType.Removed);
         public int ModifiedCount => Changes.Count(c => c.ChangeType == ChangeType.Modified);
         public int UnchangedCount => Changes.Count(c => c.ChangeType == ChangeType.Unchanged);
+        public int NoIdCount => Changes.Count(c => c.ChangeType == ChangeType.NoId);
 
     }
 }
