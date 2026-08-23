b47b25664cd66f15bda141e0881954ec9b15de2f
MahaHub <maha.mars20@gmai.com>
2026-08-23 03:44:51 -0400

    feat : implement BundleDiffProcessor.Process to build BundleDiffResult

---
+++ src/FhirDiff.Ai/BundleDiffProcessor.cs
@@ -0,0 +1,65 @@
+﻿using FhirDiff.Ai;
+using FhirDiff.Core.Models;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Channels;
+
+namespace FhirDiff.Core.Services
+{
+    public class BundleDiffProcessor
+    {
+        private readonly BundlesMatcher _matcher;
+        private readonly BundleDiffer _differ;
+        private readonly IDiffExplainer _explainer;
+
+        public BundleDiffProcessor(BundlesMatcher matcher, BundleDiffer differ, IDiffExplainer explainer)
+        {
+            _matcher = matcher;
+            _differ = differ;
+            _explainer = explainer;
+        }
+
+        public BundleDiffResult Process(Bundle oldBundle, Bundle newBundle)
+        {
+            var resourceChanges = new List<ResourceChange>();
+            var matchResults = _matcher.Match(oldBundle, newBundle);
+            
+            foreach (var addedResource in matchResults.Added) 
+            {
+                var newValue = JsonDocument.Parse(new FhirJsonSerializer().SerializeToString(addedResource.resource)).RootElement;
+                var fieldChange = new FieldDiff(addedResource.resource.TypeName, null, newValue);
+                resourceChanges.Add(new ResourceChange(addedResource.resource.TypeName, addedResource.resource.Id, ChangeType.Added, new List<FieldDiff> { fieldChange }));
+            }
+
+            foreach (var removedResource in matchResults.Removed) 
+            {
+                var oldValue = JsonDocument.Parse(new FhirJsonSerializer().SerializeToString(removedResource.resource)).RootElement;
+                var fieldChange = new FieldDiff(removedResource.resource.TypeName, oldValue, null);
+                resourceChanges.Add(new ResourceChange(removedResource.resource.TypeName, removedResource.resource.Id, ChangeType.Removed, new List<FieldDiff> { fieldChange }));
+            }
+
+            foreach (var newNonIdResource in matchResults.NewResourcesWithoutId) //non-id to be inserted as is. no diff to be called there. AddedCount increased by one
+            {
+                resourceChanges.Add(new ResourceChange(newNonIdResource.TypeName, null, ChangeType.NoId, null));
+            }
+
+            foreach (var oldNonIdResource in matchResults.OldResourcesWithoutId) //non-id to be inserted as is. no diff to be called there. AddedCount increased by one
+            {
+                resourceChanges.Add(new ResourceChange(oldNonIdResource.TypeName, null, ChangeType.NoId, null));
+            }
+
+            foreach (var matchedResource in matchResults.Matched)
+            {
+                resourceChanges.Add(_differ.Diff((matchedResource.Key, matchedResource.Old, matchedResource.New)));
+            }
+
+            var diffResults = new BundleDiffResult(resourceChanges);
+            
+            return diffResults;
+        }
+    }
+}
