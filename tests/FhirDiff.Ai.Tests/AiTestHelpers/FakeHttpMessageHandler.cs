763cd9ebb1f446d44420969aaab2844cb5951de7
MahaHub <maha.mars20@gmai.com>
2026-09-13 12:17:00 -0400

    feat : create http handler helper.

---
+++ tests/FhirDiff.Ai.Tests/AiTestHelpers/FakeHttpMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FhirDiff.Ai.Tests.AiTestHelpers
+{
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage _response;
+
+        public FakeHttpMessageHandler(HttpResponseMessage response)
+        {
+            _response = response;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_response);
+        }
+    }
+}
