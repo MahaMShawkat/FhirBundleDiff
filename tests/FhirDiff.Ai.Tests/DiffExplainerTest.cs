01b8c785952e0b411a017ea139ac4f931a923b2c
MahaHub <maha.mars20@gmai.com>
2026-09-16 12:44:41 -0400

    test: add BuildHardcodedExplanations unit test usinfg existed fixture helper.

--- tests/FhirDiff.Ai.Tests/DiffExplainerTest.cs
+++ tests/FhirDiff.Ai.Tests/DiffExplainerTest.cs
@@ -1,4 +1,5 @@
-﻿using FhirDiff.Ai.Services;
+﻿using FhirDiff.Ai.Models;
+using FhirDiff.Ai.Services;
 using FhirDiff.Ai.Tests.AiTestHelpers;
 using FhirDiff.Core.Models;
 namespace FhirDiff.Ai.Tests
@@ -26,6 +27,40 @@ namespace FhirDiff.Ai.Tests
 
             List<ResourceChange> listMatchedResources = explainer.GetMatchedResources(resourceChanges);
             Assert.Equal(new List<ResourceChange> { listResourceChanges.Modified, listResourceChanges.Unchanged }, listMatchedResources);
+        }
+
+        [Fact]
+        public void GetNonMatchedResources_MixedChangeTypes_ReturnsHardCodedResourceChangeExplanation()
+        {
+            //added, removed, and noId
+            var listResourceChanges = ResourceChangeFixtures.GetResourceChanges();
+            List<ResourceChange> resourceChanges = new List<ResourceChange> { listResourceChanges.Added, listResourceChanges.Removed, listResourceChanges.NoId };
+            GeminiDiffExplainer explainer = new GeminiDiffExplainer("fake-key");
+
+            //Act & Assert
+            List<ResourceChangeExplanation> actualExplanations = explainer.BuildHardcodedExplanations(resourceChanges);
+            List<ResourceChangeExplanation> expectedExplanations = new List<ResourceChangeExplanation>
+            {
+                new ResourceChangeExplanation ("Patient", "Patient-1", "Resource is added"),//Added
+                new ResourceChangeExplanation("Observation", "Observation-1", "Resource is removed"),//Removed
+                new ResourceChangeExplanation("Condition", null, "Resource has no Id. Matching is inapplicable")//NoId               
+            };
+
+            Assert.Equal(expectedExplanations, actualExplanations);
+
+            //Modified Changes
+            List<ResourceChange> modifiedResourceChanges = new List<ResourceChange> { listResourceChanges.Modified };
+
+            // Act & Assert
+            var modifiedException = Assert.Throws<ArgumentException>(() => explainer.BuildHardcodedExplanations(modifiedResourceChanges));
+            Assert.Equal($"Unexpected changeType '{ChangeType.Modified}' in non-matched resources.", modifiedException.Message);
+
+            //Unchanged
+            List<ResourceChange> UnchangedResourceChanges = new List<ResourceChange> { listResourceChanges.Unchanged };
+
+            // Act & Assert
+            var unchangedException = Assert.Throws<ArgumentException>(() => explainer.BuildHardcodedExplanations(UnchangedResourceChanges));
+            Assert.Equal($"Unexpected changeType '{ChangeType.Unchanged}' in non-matched resources.", unchangedException.Message);
         }
     }
 }
